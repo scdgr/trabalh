2e960e335870fd5c0e13fedf7c08ef1d26f8947c
vt0r <36089115+vt0rr@users.noreply.github.com>
2018-10-24 15:56:02 -0300

    sdgsdg

--- src/game/game.jsx
+++ src/game/game.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import './game.css'
 
+class Cavalo extends React.Component {
+    render() {
+        return (
+            <span>♘</span>
+        );
+    }
+}
+
+class Gamse extends React.Component {
+    render() {
+        const { black } = this.props;
+        const fill = black ? 'black' : 'white';
+        const stroke = black ? 'white' : 'black';
+
+        return (
+            <div style={{
+                backgroundColor: fill,
+                color: stroke,
+                width: '100%',
+                height: '100%'
+            }}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+Square.propTypes = {
+    black: PropTypes.bool
+};
+
 export default class Game extends React.Component {
     render() {
         return (
-            <div className='game'>
-                <div className='tabuleiro' /> 
-                <div className='cartas' />
-            </div>
+            <Gamse black>
+                <Cavalo />
+            </Gamse>
         )
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom'
 import App from './main/app'
 
+class Square extends Component {
+    render() {
+        const { black } = this.props;
+        const fill = black ? 'black' : 'white';
+        const stroke = black ? 'white' : 'black';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+        return (
+            <div style={{
+                backgroundColor: fill,
+                color: stroke,
+                width: '100%',
+                height: '100%'
+            }}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+Square.propTypes = {
+    black: PropTypes.bool
+};
+//ReactDOM.render(<App />, document.getElementById('app'));
